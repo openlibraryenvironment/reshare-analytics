b3d2738f3ff2a9d5b769fb3e3aa926fb969bfb26
Nassib Nassar <nrn@etymon.com>
2022-02-10 18:30:40 -0500

    Remove unused CI script

--- run-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd sql/derived_tables
-ls -l runlist.txt
-for f in $( cat runlist.txt ); do
-	if ! PGOPTIONS='--client-min-messages=warning' psql -d reshare_dev -bq -v ON_ERROR_STOP=on -c 'set search_path = reshare_derived' -f $f ; then
-		exit 1
-	fi
-done || exit 1
