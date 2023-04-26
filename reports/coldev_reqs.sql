a696e6c8c3950d3a0e74dc8a0b3ae8909217fb95
Nassib Nassar <nrn@etymon.com>
2023-04-26 13:05:15 -0400

    Various minor adjustments to reports coldev_rr and coldev_reqs

--- reports/coldev_reqs.sql
+++ reports/coldev_reqs.sql
@@ -1,39 +1,45 @@
---metadb:report coldev_reqs
+--metadb:report coldev_reqs -- Details of all requests placed by an institution's users
 --metadb:param institution text -- Name of institution as used in __origin columns
 
-CREATE FUNCTION coldev_reqs (institution text)
-    RETURNS TABLE (
-        lpr_hrid text,
-        lpr_title text,
-        lpr_author text,
-        lpr_isbn text,
-        lpr_patron_identifier text,
-        lpr_patron_email text,
-        lpr_date_created timestamp,
-        lpr_pub_date text,
-        lpr_filled int8,
-        lpr_bib_record_id text
-    )
-    AS $$
-    SELECT DISTINCT
-        *
-    FROM (
-        SELECT
-            pr.pr_hrid AS lpr_hrid,
-            pr.pr_title AS lpr_title,
-            pr.pr_author AS lpr_author,
-            pr.pr_isbn AS lpr_isbn,
-            pr.pr_patron_identifier AS lpr_patron_identifier,
-            pr.pr_patron_email AS lpr_patron_email,
-            pr.pr_date_created AS lpr_date_created,
-            pr.pr_pub_date AS lpr_pub_date,
-            lrr.lrr_filled AS lpr_filled,
-            pr.pr_bib_record_id AS lpr_bib_record_id
-        FROM
-            reshare_rs.patron_request pr
-        LEFT JOIN (SELECT * FROM coldev_rr(institution)) AS lrr ON pr.pr_id = lrr.lrr_req_id::uuid
-    WHERE
-        pr.pr_is_requester = TRUE
-        AND pr."__origin" = institution) AS coldevdata
+CREATE FUNCTION coldev_reqs(institution text)
+    RETURNS TABLE( lpr_hrid text,
+                   lpr_title text,
+                   lpr_author text,
+                   lpr_isbn text,
+                   lpr_patron_identifier text,
+                   lpr_patron_email text,
+                   lpr_date_created timestamp,
+                   lpr_pub_date text,
+                   lpr_filled int8,
+                   lpr_bib_record_id text
+                 ) AS
+$$
+SELECT DISTINCT ( lpr_hrid,
+                  lpr_title,
+                  lpr_author,
+                  lpr_isbn,
+                  lpr_patron_identifier,
+                  lpr_patron_email,
+                  lpr_date_created,
+                  lpr_pub_date,
+                  lpr_filled,
+                  lpr_bib_record_id
+                )
+    FROM ( SELECT pr.pr_hrid AS lpr_hrid,
+                  pr.pr_title AS lpr_title,
+                  pr.pr_author AS lpr_author,
+                  pr.pr_isbn AS lpr_isbn,
+                  pr.pr_patron_identifier AS lpr_patron_identifier,
+                  pr.pr_patron_email AS lpr_patron_email,
+                  pr.pr_date_created AS lpr_date_created,
+                  pr.pr_pub_date AS lpr_pub_date,
+                  lrr.lrr_filled AS lpr_filled,
+                  pr.pr_bib_record_id AS lpr_bib_record_id
+           FROM reshare_rs.patron_request AS pr
+               LEFT JOIN ( SELECT lrr_req_id::uuid, lrr_filled
+                               FROM coldev_rr(institution)
+                         ) AS lrr ON pr.pr_id = lrr.lrr_req_id
+           WHERE pr.pr_is_requester AND pr.__origin = institution
+         ) AS coldevdata
 $$
 LANGUAGE SQL;
--- reports/coldev_rr.sql
+++ reports/coldev_rr.sql
@@ -1,27 +1,17 @@
---metadb:report coldev_rr
+--metadb:report coldev_rr -- All requests placed by an institution's users and if the request was fulfilled
 --metadb:param institution text -- Name of institution as used in __origin columns
 
-CREATE FUNCTION coldev_rr (institution text)
-    RETURNS TABLE (
-        lrr_req_id text,
-        lrr_filled int8
-    )
-    AS $$
-    SELECT
-        rs.rs_req_id AS lrr_req_id,
-        sum(
-            CASE WHEN (rs_from_status = 'REQ_SHIPPED'
-                AND rs_to_status = 'REQ_CHECKED_IN')
-                OR rs_to_status = 'REQ_FILLED_LOCALLY' THEN
-                1
-            ELSE
-                0
-            END) AS lrr_filled
-    FROM
-        reshare_derived.req_stats AS rs
-    WHERE
-        rs.rs_requester = institution
-    GROUP BY
-        rs.rs_req_id
+CREATE FUNCTION coldev_rr(institution text)
+    RETURNS TABLE(lrr_req_id text, lrr_filled int8) AS
+$$
+SELECT rs.rs_req_id AS lrr_req_id,
+       sum( CASE WHEN (rs_from_status = 'REQ_SHIPPED' AND rs_to_status = 'REQ_CHECKED_IN') OR rs_to_status = 'REQ_FILLED_LOCALLY'
+                     THEN 1
+                 ELSE 0
+            END
+          ) AS lrr_filled
+    FROM reshare_derived.req_stats AS rs
+    WHERE rs.rs_requester = institution
+    GROUP BY rs.rs_req_id
 $$
 LANGUAGE SQL;
